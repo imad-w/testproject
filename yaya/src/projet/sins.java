543e00fd12b60bbeca97a22354f035cea71dbfda
TEST <TEST@DESKTOP-G3GPHMU>
2020-11-15 20:07:42 +0100

    sins

--- yaya/src/projet/sins.java
+++ yaya/src/projet/sins.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import pam.ConnexionMysql;
+
 
 /**
  * Servlet implementation class sins
